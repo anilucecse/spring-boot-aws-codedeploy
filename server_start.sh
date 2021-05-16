9b59e1d1391823c2bfabaac0828bb616c76fc6f9
Nayak, Anil Kumar <anilkumar.nayak@finastra.com>
2021-05-16 23:14:56 +0530

    port changed

--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=8080 \
+sudo /usr/bin/java -jar -Dserver.port=80 \
     aws-code-pipeline.jar > /dev/null 2> /dev/null < /dev/null &
